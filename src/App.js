7686a8bfa0f58ef4812e09810d1553dcf33b0559
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-06-21 10:05:13 +0530

    Routing Completed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,37 @@ import {
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
+import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+
+const Layout= () =>{
+  return(
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home/>
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>
+      },
+    ]
   },
   {
     path:"/products/:id",
