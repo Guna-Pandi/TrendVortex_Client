436f7dc84ab100c88228f07a5ceedfa58587eb02
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-07-10 07:52:03 +0530

    Started :Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
   Outlet,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    // Navbar
+    <div className=" ">
+      {/* Wrapper */}
+      <div className=" ">
+        {/* left */}
+        <div>
+          <div>
+            <img src="img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+        </div>
 
-export default Navbar
+        {/* center */}
+        <div></div>
+        {/* Right */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
