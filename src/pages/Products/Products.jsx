cdd93b60612ac257736104fb6f86c457f7dbcc5a
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-09-17 10:03:11 +0530

    Feat : Products,List

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,95 +1,60 @@
-// import React from "react";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
-// import List from "../../components/List/List";
-// import useFetch from "../../hooks/useFetch";
-// import "./Products.scss";
+import React from "react";
+import List from './../../components/List/List';
 
-// const Products = () => {
-//   const catId = parseInt(useParams().id);
-//   const [maxPrice, setMaxPrice] = useState(1000);
-//   const [sort, setSort] = useState(null);
-//   const [selectedSubCats, setSelectedSubCats] = useState([]);
+const Products = () => {
+  return (
+    // products
+    <div className="">
+      {/* left */}
+      <div>
+        {/* filter item */}
+        <div>
+          <h2>Product Categories</h2>
+          <div>
+            <input type="checkbox" value={1} id="1" />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div>
+            <input type="checkbox" value={2} id="2" />
+            <label htmlFor="2">Skirts</label>
+          </div>
+          <div>
+            <input type="checkbox" value={3} id="3" />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
 
-//   const { data, loading, error } = useFetch(
-//     `/sub-categories?[filters][categories][id][$eq]=${catId}`
-//   );
+        {/* Filter by price */}
+        <div>
+          <h2>Filter by price</h2>
+          <div>
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
 
-//   const handleChange = (e) => {
-//     const value = e.target.value;
-//     const isChecked = e.target.checked;
+        {/* sort by price */}
+        <div>
+          <h2>Sort by price</h2>
+          <div>
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price(Lowest First)</label>
+          </div>
+          <div>
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price(Highest First)</label>
+          </div>
+        </div>
+      </div>
 
-//     setSelectedSubCats(
-//       isChecked
-//         ? [...selectedSubCats, value]
-//         : selectedSubCats.filter((item) => item !== value)
-//     );
-//   };
+      {/* right */}
+      <div>
+        <img src="https://images.unsplash.com/photo-1682687219356-e820ca126c92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
+        <List/>
+      </div>
+    </div>
+  );
+};
 
-//   return (
-//     <div className="products">
-//       <div className="left">
-//         <div className="filterItem">
-//           <h2>Product Categories</h2>
-//           {data?.map((item) => (
-//             <div className="inputItem" key={item.id}>
-//               <input
-//                 type="checkbox"
-//                 id={item.id}
-//                 value={item.id}
-//                 onChange={handleChange}
-//               />
-//               <label htmlFor={item.id}>{item.attributes.title}</label>
-//             </div>
-//           ))}
-//         </div>
-//         <div className="filterItem">
-//           <h2>Filter by price</h2>
-//           <div className="inputItem">
-//             <span>0</span>
-//             <input
-//               type="range"
-//               min={0}
-//               max={1000}
-//               onChange={(e) => setMaxPrice(e.target.value)}
-//             />
-//             <span>{maxPrice}</span>
-//           </div>
-//         </div>
-//         <div className="filterItem">
-//           <h2>Sort by</h2>
-//           <div className="inputItem">
-//             <input
-//               type="radio"
-//               id="asc"
-//               value="asc"
-//               name="price"
-//               onChange={(e) => setSort("asc")}
-//             />
-//             <label htmlFor="asc">Price (Lowest first)</label>
-//           </div>
-//           <div className="inputItem">
-//             <input
-//               type="radio"
-//               id="desc"
-//               value="desc"
-//               name="price"
-//               onChange={(e) => setSort("desc")}
-//             />
-//             <label htmlFor="desc">Price (Highest first)</label>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="right">
-//         <img
-//           className="catImg"
-//           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-//           alt=""
-//         />
-//         <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats}/>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Products;
+export default Products;
