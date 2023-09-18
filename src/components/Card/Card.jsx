51a534355d3a43216fb9010eecf3d0aba2edc48b
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-09-19 00:04:20 +0530

    Feat:env,card,featuredproducts

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -42,30 +42,34 @@ const Card = ({ item }) => {
     <Link to={`/product/${item.id}`}>
       <div className=" w-[220px] flex flex-col gap-3 mb-12 mr-1">
         <div className="w-full h-[400px] overflow-hidden relative group">
-          {item.isNew && (
+          {item?.attributes.isNew && (
             <span className=" absolute top-1 left-1 text-teal-500 px-[5px] py-1 z-[3] bg-slate-200 text-xs font-medium">
               {" "}
               New Season
             </span>
           )}
           <img
-            src={item.img}
+            src={
+              process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url
+            }
             alt=""
             className="w-full h-full object-cover absolute transition-opacity duration-300"
           />
           <img
-            src={item.img2}
+            src={
+              process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2?.data?.attributes?.url
+            }
             alt=""
             className="w-full h-full object-cover absolute opacity-0 transition-opacity duration-300 group-hover:opacity-100"
           />
         </div>
 
-        <h2 className=" text-base font-normal">{item.title}</h2>
+        <h2 className=" text-base font-normal">{item?.attributes.title}</h2>
         <div className=" flex gap-5">
           <h3 className=" text-lg font-medium text-gray-400 line-through">
-            ${item.oldPrice}
+            ${item.oldPrice || item?.attributes.price + 30}
           </h3>
-          <h3 className=" text-lg font-medium ">${item.price}</h3>
+          <h3 className=" text-lg font-medium ">${item?.attributes.price}</h3>
         </div>
       </div>
     </Link>
--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,46 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
-// import useFetch from "../../hooks/useFetch";
+import axios from "axios";
 
 const FeaturedProducts = ({ type }) => {
-  //   const { data, loading, error } = useFetch(
-  //     `/products?populate=*&[filters][type][$eq]=${type}`
-  //   );
-  const data = [
-    {
-      id: 1,
-      img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=420&q=80",
-      img2: "https://images.unsplash.com/photo-1601762603339-fd61e28b698a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-      title: "Long sleve graphic t shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 11,
-    },
-    {
-      id: 2,
-      img: "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80",
-      title: "Long sleve graphic t shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 11,
-    },
-    {
-      id: 3,
-      img: "https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80",
-      title: "Long sleve graphic t shirt",
-      isNew: false,
-      oldPrice: 19,
-      price: 11,
-    },
-    {
-      id: 4,
-      img: "https://plus.unsplash.com/premium_photo-1673758905770-a62f4309c43c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-      title: "Long sleve graphic t shirt",
-      isNew: false,
-      oldPrice: 19,
-      price: 11,
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setData(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="my-[100px] mx-[200px]">
@@ -55,14 +37,6 @@ const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className=" flex justify-center gap-12">
-        {/* {error
-          ? "Something went wrong!"
-          : loading
-          ? "loading"
-          : 
-          data?.map((item) =>
-           <Card item={item}
-            key={item.id} />)} */}
         {data.map((item) => (
           <Card item={item} key={item.id} />
         ))}
